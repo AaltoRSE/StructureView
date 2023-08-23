a422d1f672d7a82eec2d7bb089ac0939dbe88458
Thomas Pfau <github@thomaspfau.de>
2023-08-23 12:33:18 +0300

    Fixing behaviour when reused

--- src/components/utils/FixedCheckBox.vue
+++ src/components/utils/FixedCheckBox.vue
@@ -12,6 +12,11 @@ export default {
       placeHolder: this.isChecked // Set this to the property's value
     }
   },
+  watch: {
+    isChecked(newValue) {
+      this.placeHolder = newValue
+    }
+  },
   components: { Checkbox },
   props: {
     isChecked: Boolean, // Assuming the property is a Boolean
